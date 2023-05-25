7d2a5356140361ce8dd8eb5ef583dd9619a07be2
Parul Bhardwaj <bhardwaj.parul88@gmail.com>
2023-05-25 13:17:35 -0400

    capitalize words

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -1,24 +1,16 @@
 import random
 import sys
 
-# When sys.exit(1) is executed, it terminates the script and returns an exit status of 1, 
-# which typically signifies an abnormal or error termination. 
 def dictionary_word_sentence_generation(number_words):
     with open("/usr/share/dict/words", encoding="utf-8") as a_file:
         words = a_file.read().split()
-    if number_words > len(words):
-        print("Error: Requested number of words exceeds the available word count.")
-        sys.exit(1)
 
     random_words = random.sample(words, number_words)
+    random_words[0] = random_words[0].capitalize()
     sentence = ' '.join(random_words)
     return sentence
 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python3 dictionary_words.py <num_words>")
-        sys.exit(1)
-
     num_words = int(sys.argv[1])
     sentence = dictionary_word_sentence_generation(num_words)
     print(sentence)
